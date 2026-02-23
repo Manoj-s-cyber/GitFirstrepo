79b31f49d8b2323f99068370267065704698bbef
Manoj-s-cyber <manojksunia@gmail.com>
2026-02-24 05:06:38 +0530

    Delete Assignment_Task2.py

--- Assignment_Task2.py
+++
@@ -1,5 +0,0 @@
-first_name= input("Enter first name: ")
-last_name = input("Enter last name: ")
-full_name= first_name + " " + last_name
-print(full_name)
-print("Hello ",first_name,last_name,"!","Welcome to the python program")
